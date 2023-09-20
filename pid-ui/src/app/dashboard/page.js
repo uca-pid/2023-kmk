e5831c3dbee0042516d42563a1154ee46655375d
aguskrembs <72949652+aguskrembs@users.noreply.github.com>
2023-09-20 17:52:04 -0300

    Added appointments form

--- pid-ui/src/app/dashboard/page.js
+++ pid-ui/src/app/dashboard/page.js
@@ -8,31 +8,65 @@ import { useRouter } from "next/navigation";
 const Dashboard = () => {
   const router = useRouter();
   const [especialidad, setEspecialidad] = useState("");
-  const [doctor, setDoctor] = useState("");
+  // Estado para almacenar la lista de especialidades
+  const [especialidades, setEspecialidades] = useState([]);
+  // Estado para almacenar la lista de médicos
+  const [medicos, setMedicos] = useState([]);
+  // Estado para almacenar los turnos disponibles
   const [turnosDisponibles, setTurnosDisponibles] = useState([]);
+  // Estado para el valor seleccionado de la especialidad
+  const [selectedEspecialidad, setSelectedEspecialidad] = useState("");
+  // Estado para el valor seleccionado del médico
+  const [selectedMedico, setSelectedMedico] = useState("");
 
-  // Función para cargar los turnos disponibles cuando cambian especialidad o doctor
-  const cargarTurnosDisponibles = async () => {
-    try {
-      // Realiza una solicitud al backend para obtener los turnos disponibles
-      const response = await fetch(
-        `/api/turnos?especialidad=${especialidad}&doctor=${doctor}`
-      );
-      if (response.ok) {
-        const data = await response.json();
-        setTurnosDisponibles(data.turnos);
-      } else {
-        console.error("Error al cargar los turnos disponibles.");
-      }
-    } catch (error) {
-      console.error("Error al cargar los turnos disponibles:", error);
-    }
-  };
-
+  // Efecto para cargar las especialidades al cargar la página
   useEffect(() => {
-    // Cargar los turnos disponibles cuando cambian especialidad o doctor
-    cargarTurnosDisponibles();
-  }, [especialidad, doctor]);
+    // Llamada a la API para obtener la lista de especialidades
+    fetch("/api/especialidades")
+      .then((response) => response.json())
+      .then((data) => {
+        setEspecialidades(data);
+      })
+      .catch((error) => {
+        console.error("Error al obtener las especialidades:", error);
+      });
+  }, []);
+
+  // Efecto para cargar los médicos cuando se selecciona una especialidad
+  useEffect(() => {
+    if (selectedEspecialidad) {
+      // Llamada a la API para obtener la lista de médicos para la especialidad seleccionada
+      fetch(`/api/medicos?especialidad=${selectedEspecialidad}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setMedicos(data);
+        })
+        .catch((error) => {
+          console.error("Error al obtener los médicos:", error);
+        });
+    } else {
+      // Si no se selecciona una especialidad, borra la lista de médicos
+      setMedicos([]);
+    }
+  }, [selectedEspecialidad]);
+
+  // Efecto para cargar los turnos disponibles cuando se selecciona un médico
+  useEffect(() => {
+    if (selectedMedico) {
+      // Llamada a la API para obtener los turnos disponibles para el médico seleccionado
+      fetch(`/api/turnos?medico=${selectedMedico}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setTurnosDisponibles(data);
+        })
+        .catch((error) => {
+          console.error("Error al obtener los turnos disponibles:", error);
+        });
+    } else {
+      // Si no se selecciona un médico, borra la lista de turnos disponibles
+      setTurnosDisponibles([]);
+    }
+  }, [selectedMedico]);
 
   const handleLogoClick = () => {
     router.push("/dashboard");
@@ -44,35 +78,45 @@ const Dashboard = () => {
         <img src="/logo.png" alt="Logo de la empresa" className={styles.logo} />
       </header>
 
-      {/* Barra superior de navegación por pestañas */}
       <div className={styles["tab-bar"]}>
-        <div className={styles.tab} onClick={router.push("/dashboard")}>
+        <div className={styles.tab} onClick={handleLogoClick}>
           Turnos
         </div>
-        <div className={styles.disabledtab}>Mi Ficha (Coming soon)</div>
-
-        {/* Agrega más pestañas según tus necesidades */}
+        <div className={styles.tab_disabled}>Mi Ficha (Coming soon)</div>
       </div>
 
-      {/* Contenido de la pestaña "Turnos" */}
       <div className={styles["tab-content"]}>
         {/* Formulario de selección de especialidad y doctor */}
         <div className={styles.form}>
+          {/* Selector de especialidades */}
           <label htmlFor="especialidad">Especialidad:</label>
           <select
             id="especialidad"
-            value={especialidad}
-            onChange={(e) => setEspecialidad(e.target.value)}
+            value={selectedEspecialidad}
+            onChange={(e) => setSelectedEspecialidad(e.target.value)}
           >
-            {/* Opciones de especialidad */}
+            <option value="">Selecciona una especialidad</option>
+            {especialidades.map((especialidad) => (
+              <option key={especialidad.id} value={especialidad.id}>
+                {especialidad.nombre}
+              </option>
+            ))}
           </select>
-          <label htmlFor="doctor">Doctor:</label>
+
+          {/* Selector de médicos */}
+          <label htmlFor="medico">Médico:</label>
           <select
-            id="doctor"
-            value={doctor}
-            onChange={(e) => setDoctor(e.target.value)}
+            id="medico"
+            value={selectedMedico}
+            onChange={(e) => setSelectedMedico(e.target.value)}
+            disabled={!selectedEspecialidad} // Deshabilita si no se ha seleccionado una especialidad
           >
-            {/* Opciones de doctor */}
+            <option value="">Selecciona un médico</option>
+            {medicos.map((medico) => (
+              <option key={medico.id} value={medico.id}>
+                {medico.nombre}
+              </option>
+            ))}
           </select>
         </div>
 
