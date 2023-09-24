f1f12b4db7cdfa5d9ef33926540f3805dd7f4005
FelicitasKlix <felikpc@gmail>
2023-09-24 20:15:38 -0300

    register endpoint

--- pid-be/app/models/entities/Physician.py
+++ pid-be/app/models/entities/Physician.py
@@ -4,6 +4,38 @@ db = firestore.client()
 
 
 class Physician:
+    role: str
+    name: str
+    last_name: str
+    matricula: int
+    specialty: str
+    email: str
+    password: str
+    id: str
+    approved: str
+
+    def __init__(
+        self,
+        role: str,
+        name: str,
+        last_name: str,
+        matricula: int,
+        specialty: str,
+        email: str,
+        password: str,
+        id: str,
+        approved: str,
+    ):
+        self.role = role
+        self.name = name
+        self.last_name = last_name
+        self.matricula = matricula
+        self.specialty = specialty
+        self.email = email
+        self.password = password
+        self.id = id
+        self.approved = approved
+
     @staticmethod
     def exists_physician_with(id):
         physician_document = db.collection("physicians").document(id).get()
@@ -12,3 +44,18 @@ class Physician:
     @staticmethod
     def get_by_id(id):
         return db.collection("physicians").document(id).get().to_dict()
+
+    def create(self):
+        id = db.collection("physicians").document().id
+        db.collection("physicians").document(id).set(
+            {
+                "id": self.id,
+                "name": self.name,
+                "last_name": self.last_name,
+                "matricula": self.matricula,
+                "specialty": self.specialty,
+                "email": self.email,
+                "approved": self.approved,
+            }
+        )
+        return self.id
--- pid-be/app/models/requests/UserRequests.py
+++ pid-be/app/models/requests/UserRequests.py
@@ -6,3 +6,21 @@ from fastapi import Query
 class UserLoginRequest(BaseModel):
     email: Annotated[str, Query(regex="^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$")]
     password: str
+
+
+class PhysicianRegisterRequest(BaseModel):
+    role: str
+    name: str
+    last_name: str
+    matricula: str
+    specialty: str
+    email: Annotated[str, Query(regex="^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$")]
+    password: str
+
+
+class PatientRegisterRequest(BaseModel):
+    role: str
+    name: str
+    last_name: str
+    email: Annotated[str, Query(regex="^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$")]
+    password: str
