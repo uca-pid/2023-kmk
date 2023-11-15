61effd3f068fb67b68aea441fc5a2855ff7b7bf3
aguskrembs <72949652+aguskrembs@users.noreply.github.com>
2023-11-15 00:02:44 -0300

    styles fix

--- pid-ui/src/app/physician-agenda/page.js
+++ pid-ui/src/app/physician-agenda/page.js
@@ -93,6 +93,18 @@ const PhysicianAgenda = () => {
         }
     };
 
+    const customStyles = {
+        content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            width: "80%",
+        },
+    };
+
     const handleCloseEditModal = () => {
         setIsAddObervationModalOpen(false);
     };
