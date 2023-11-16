e74e67224ba76b6b3ae5bfee907c540ba9d885e7
tommudano <38632202+tommudano@users.noreply.github.com>
2023-11-16 17:36:23 -0300

    added front ui handling

--- pid-be/tests/test_update_physicians_agenda.py
+++ pid-be/tests/test_update_physicians_agenda.py
@@ -129,3 +129,54 @@ def test_agenda_gets_changed_by_removing_day():
     assert len(physician_doc["agenda"].keys()) == 1
     assert physician_doc["agenda"][str(number_of_day_of_week)]["start"] == 8
     assert physician_doc["agenda"][str(number_of_day_of_week)]["finish"] == 19.5
+
+
+def test_request_to_physician_schedule_update_with_finishing_time_less_than_initial_time_returns_422_code_and_detail():
+    response_from_physician_schedule_update_endpoint = client.put(
+        "/physicians/agenda",
+        headers={"Authorization": f"Bearer {pytest.physician_bearer_token}"},
+        json={
+            str(number_of_day_of_week): {"start": 20, "finish": 19.5},
+            str((number_of_day_of_week + 1) % 7): {"start": 8, "finish": 9},
+        },
+    )
+
+    assert response_from_physician_schedule_update_endpoint.status_code == 422
+    assert (
+        response_from_physician_schedule_update_endpoint.json()["detail"]
+        == "Invalid input format"
+    )
+
+
+def test_request_to_physician_schedule_update_with_negative_finishing_time_returns_422_code_and_detail():
+    response_from_physician_schedule_update_endpoint = client.put(
+        "/physicians/agenda",
+        headers={"Authorization": f"Bearer {pytest.physician_bearer_token}"},
+        json={
+            str(number_of_day_of_week): {"start": 0, "finish": -19.5},
+            str((number_of_day_of_week + 1) % 7): {"start": 8, "finish": 9},
+        },
+    )
+
+    assert response_from_physician_schedule_update_endpoint.status_code == 422
+    assert (
+        response_from_physician_schedule_update_endpoint.json()["detail"]
+        == "Invalid input format"
+    )
+
+
+def test_request_to_physician_schedule_update_with_negative_start_time_returns_422_code_and_detail():
+    response_from_physician_schedule_update_endpoint = client.put(
+        "/physicians/agenda",
+        headers={"Authorization": f"Bearer {pytest.physician_bearer_token}"},
+        json={
+            str(number_of_day_of_week): {"start": -1, "finish": 19.5},
+            str((number_of_day_of_week + 1) % 7): {"start": 8, "finish": 9},
+        },
+    )
+
+    assert response_from_physician_schedule_update_endpoint.status_code == 422
+    assert (
+        response_from_physician_schedule_update_endpoint.json()["detail"]
+        == "Invalid input format"
+    )
